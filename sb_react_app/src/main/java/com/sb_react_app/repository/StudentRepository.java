710423b69f361741cf0e6c9bbdaa76bae846e9e1
Jose Ochoa <joseochoa@Mac.lan>
2025-12-18 17:34:59 -0500

    Modified StudentRepository to include findByEmail method.

--- sb_react_app/src/main/java/com/sb_react_app/repository/StudentRepository.java
+++ sb_react_app/src/main/java/com/sb_react_app/repository/StudentRepository.java
@@ -1,7 +1,11 @@
 package com.sb_react_app.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.sb_react_app.model.Student;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    Optional<Student> findByEmail(String email);
 }
