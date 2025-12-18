b2150b1db51315f019972c04acc4673e04cf38aa
Jose Ochoa <joseochoa@Mac.lan>
2025-12-18 16:33:18 -0500

    Initial commit, project creation for student-profile app.

---
+++ sb_react_app/src/main/java/com/sb_react_app/SbReactAppApplication.java
@@ -0,0 +1,13 @@
+package com.sb_react_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbReactAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbReactAppApplication.class, args);
+	}
+
+}
---
+++ sb_react_app/src/test/java/com/sb_react_app/SbReactAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb_react_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbReactAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
